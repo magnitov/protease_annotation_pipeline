4c8afe4b72bb2ed86dd0ca332e89ce1e5bdaf5ea
magnitov <mikhail.magnitov@phystech.edu>
2020-11-11 13:52:55 +0300

    database annotation workflow added

--- README.md
+++ README.md
@@ -1,1 +1,76 @@
-# protease_annotation_pipeline
+# Annotation of protease structures
+
+Rodrigo Ochoa, Mikhail Magnitov, Roman A Laskowski, Pilar Cossio, Janet M Thornton. *"Modelling peptide substrates bound to proteases: insights to study binding recognition"*. ***Manuscript under the review***
+
+## Purpose
+
+The goal of these scripts is to provide an example workflow of the protease-peptide structure annotation prior to the modelling step described in [https://github.com/rochoa85/Modelling-Protease-Substrates](https://github.com/rochoa85/Modelling-Protease-Substrates). The first script is focused on the annotation of the proteases with necessary information from publicly available resources. The second script allows the annotation and assignment of the binding pockets to each substrate residue using the reference substrate, and projecting the obtained pockets to other target substrates.
+
+## Extra files required
+
+To perform the annotation of the ligand and protein chains, two files from the PDBsum database are required:
+
+1. rasmol.dfn file that contains the information on ligand chain and residues. This file is accessible for each PDB entry via https://www.ebi.ac.uk/thornton-srv/databases/pdbsum/PDB_code/rasmol.dfn
+
+2. grow.out file that contains the information on protein chains and residues bound to ligand. This file is accessible for each PDB entry via https://www.ebi.ac.uk/thornton-srv/databases/pdbsum/PDB_code/grow.out
+
+**NOTE:** Put these files into the annotation folder (see repository for an example).
+
+To perform the annotation of the IDs and catalytic residues, two files from the PDBsum and M-CSA databases are required:
+
+1. Annotation of PDB structures with EC numbers and UniProt IDs. This file can be downloaded from: [http://www.ebi.ac.uk/thornton-srv/databases/pdbsum/data/pdb_chain_sp_ec](http://www.ebi.ac.uk/thornton-srv/databases/pdbsum/data/pdb_chain_sp_ec) (36.7 Mb)
+
+2. Information about the catalytic residues, including the residues found by homology. This file can be downloaded from: [https://www.ebi.ac.uk/thornton-srv/m-csa/api/homologues_residues.json](https://www.ebi.ac.uk/thornton-srv/m-csa/api/homologues_residues.json) (295.4 Mb)
+
+## Implementation
+
+#### Step 1: Annotating the dataset
+First, proteases are annotated with ligand and protein PDB chains, EC number, UniProt and MEROPS IDs. The search for EC number and UniProt ID is performed using the obtained PDBsum database file, and the search for MEROPS ID is performed via UniProt REST API. Second, the protein chains bound to the ligand are annotated with a list of the catalytic residues. This step is performed using the obtained M-CSA database file.
+
+**NOTE:** If any protein chains are not annotated with catalytic residues, one can perform a homology search using the protease sequence in order to infer them.
+
+The basic command line to run the annotation script is:
+
+```
+python run_annotation.py --input INPUT [--output OUTPUT]
+
+arguments:
+  --input INPUT    Dataset with protease-ligand pairs to annotate
+  --output OUTPUT  Path to save the annotated dataset
+```
+
+#### Step 2: Identifying pockets
+To identify the pocket residues, where the substrates bind to the protease, we use the coordinates of the reference substrate residues within P4-P4’ pockets. The reference substrate chosen should be stretched across pockets S4-S4’, or, failing that, the longest available. Protease residues with at least one atom within a defined from the substrate are considered to form a binding pocket. We then project the annotation from the reference substrate to target substrates to assign each substrate residue a pocket where it binds.
+
+**NOTE:** This analysis requires two additional steps. The first is selecting a reference structure. The second in superimposing of the proteases of interest with a selected reference structure (we suggest using PDBeFold) and obtaining new superimposed coordinates of the structures.
+
+## Examples
+
+#### Step 1: Annotating the dataset
+
+For this step we provided a sample data of 7 proteases from major protease families as an example in **sample_data_annotation.csv**. It includes a list of PDB codes followed by ligand sequence:
+
+```
+3tjv,PRO-THR-SER-TYR-ALA-GLY-ASP-ASP-SER
+```
+
+The following command will do the annotation: `python run_annotation.py --input sample_data_annotation.csv`
+
+After running this comand, the annotated dataset will be saved to **annotated_dataset.csv**. It will include to following fields:
+
+```
+PDB: 3tjv
+Ligand: PRO-THR-SER-TYR-ALA-GLY-ASP-ASP-SER
+Ligand_size: 9
+Ligand_chain: B
+Ligand_residues_number: 1, 2, 3, 4, 5, 6, 7, 8, 9
+Protein_chains: A
+Protein_residues: A100PHE, A144TYR, A150LEU, A174ASN, A192GLY, A193PHE, A194LYS, A195GLY, A196ASP, A197SER, A212SER, A213TYR, A214GLY, A215ASN, A216LYS, A218GLY, A32PHE, A34GLN, A41ARG, A42LYS, A43ARG, A44CYS, A59HIS, A75LYS
+EC_number: 3.4.21.-
+UniProt_ID: P20718
+MEROPS_ID: S01.147
+Catalytic_residues: A103ASN, A191THR, A194LYS, A195GLY, A196ASP, A197SER, A198GLY, A59HIS, A99ASN
+```
+
+#### Step 2: Identifying pockets in reference protease structure
+
---
+++ run_annotation.py
@@ -0,0 +1,239 @@
+#!/usr/bin/python3
+
+########################################################################################
+# Authorship
+########################################################################################
+
+__author__ = "Mikhail Magnitov"
+__email__ = "mikhail.magnitov@phystech.edu"
+
+########################################################################################
+# Modules to import
+########################################################################################
+
+import argparse
+import pandas as pd
+import numpy as np
+import os
+import re
+from urllib import parse, request
+import warnings
+warnings.filterwarnings('ignore')
+
+########################################################################################
+# Functions
+########################################################################################
+
+def find_ligand(ligand, rasmol_file):
+    """
+    Function that finds ligand chain and ligand residue numbers
+    """
+
+    ligand_chain = ""
+    residues_numbers = []
+    length_ligand = len(ligand)
+    for row in rasmol_file:
+        if row[0] == "L" and all(ligand_iter in row for ligand_iter in ligand) and ':' in row:
+            row = row.replace(",", "").replace("[", "").replace("]", "").split()
+            del row[0]
+            ligand_str = "".join(ligand)
+            ligand_str_row = "".join((iter[0:3] for iter in row))
+            if ligand_str_row == ligand_str:
+                for i in range(len(row)):
+                    if row[i][-3].isalpha():
+                        row[i] = row[i][:-3] + row[i][-2:]
+                ligand_chain = row[0][-1]
+                residues_numbers = [int(row[i][3:-2]) for i in range(length_ligand)]
+                break
+        else: # the cases when ligand chain is missing from the rasmol file
+            ligand_chain = ''
+            residues_numbers = np.nan
+    return (residues_numbers, ligand_chain)
+
+##################################################################################
+
+def find_prot_chain_bound_to_ligand(ligand, residue_number, ligand_chain, grow_out_file):
+    """
+    Function that finds protein chains that are bound to ligand
+    """
+
+    chains_bound_to_ligand = []
+    for row in grow_out_file:
+        if row[14:17] == row[14:17].upper() and row[54] == ligand_chain\
+           and (row[44:47], int(row[49:53])) in zip(ligand, residue_number):
+            if row[6] not in chains_bound_to_ligand:
+                chains_bound_to_ligand.append(row[6])
+    return (chains_bound_to_ligand)
+
+##################################################################################
+
+def find_prot_residues_bound_to_ligand(ligand, residue_number, ligand_chain, grow_out_file):
+    """
+    Function that finds a list of residues that are bound to ligand
+    """
+
+    residues_bound_to_ligand = []
+    for row in grow_out_file:
+        if row[14:17] == row[14:17].upper() and row[54] == ligand_chain\
+           and (row[44:47], int(row[49:53])) in zip(ligand, residue_number):
+            if (row[5:17].replace(' ', '') not in residues_bound_to_ligand) and len(re.findall('\d+', row[5:17].replace(' ', '')[-3:])) == 0:
+                residues_bound_to_ligand.append(row[5:17].replace(' ', ''))
+    return (residues_bound_to_ligand)
+
+##################################################################################
+
+def find_chains(data):
+    """
+    Function that combined the search for ligand and protein chain and residues
+    """
+
+    # Length of the ligand
+    data['Ligand_size'] = [len(x.split('-')) for x in data['Ligand'].values]
+
+    ligand_chain, ligand_residues, bound_chains, bound_residues = [], [], [], []
+    for j, PDB, ligand in zip(data.index, data['PDB'], [x.split('-') for x in data['Ligand'].values]):
+        # Find ligand position
+        with open('./annotation/rasmol/' + PDB + '_rasmol.dfn', 'r') as file_ligand:
+            ligand_residues_tmp, ligand_chain_tmp = find_ligand(ligand, file_ligand)
+        ligand_chain.append(ligand_chain_tmp)
+        ligand_residues.append(ligand_residues_tmp)
+
+        # Find protein chains bound to ligand
+        with open('./annotation/grow/' + PDB + '_grow.out', 'r') as grow_file:
+            bound_chains_tmp = find_prot_chain_bound_to_ligand(ligand, ligand_residues_tmp, ligand_chain_tmp, grow_file)
+            if len(bound_chains_tmp) == 0:
+                bound_chains_tmp = ''
+        bound_chains.append(bound_chains_tmp)
+
+        # Find protein residues bound to ligand
+        with open('./annotation/grow/' + PDB + '_grow.out', "r") as grow_file:
+            bound_residues_tmp = find_prot_residues_bound_to_ligand(ligand, ligand_residues_tmp, ligand_chain_tmp, grow_file)
+            if len(bound_residues_tmp) == 0:
+                bound_residues_tmp = ''
+            else:
+                bound_residues_tmp = list(np.unique(bound_residues_tmp))
+        bound_residues.append(bound_residues_tmp)
+
+    data['Ligand_chain'] = ligand_chain
+    data['Ligand_residues_number'] = ligand_residues
+    data['Protein_chains'] = bound_chains
+    data['Protein_residues'] = bound_residues
+    return(data)
+
+##################################################################################
+
+def find_ids(data):
+    """
+    Function that implements the annotation of EC number, UniProt ID and MEROPS ID
+    """
+
+    # Retrieving EC and UniProt IDs from PDBsum annotation file
+    ec = pd.read_table('pdb_chain_sp_ec', header = None, sep = '\s', engine = 'python')
+    for i in [2, 4, 6, 8, 10]:
+        ec[i] = [x.replace('"', '') for x in ec[i].values]
+
+    ecs, uniprots = [], []
+    for i in range(0, len(data)):
+        ec_temp, uniprot_temp = [], []
+        chains_take = str(data['Protein_chains'].values[i]).replace('[', '')\
+                         .replace(']', '').replace("'", "").split(', ')
+        for j in range(0, len(ec[ec[2] == data['PDB'].values[i]])):
+            if ec[ec[2] == data['PDB'].values[i]][4].values[j] in chains_take:
+                ec_temp.append(ec[ec[2] == data['PDB'].values[i]][10].values[j])
+                uniprot_temp.append(ec[ec[2] == data['PDB'].values[i]][6].values[j])
+
+        ec_temp = ', '.join(np.unique(ec_temp))
+        if ec_temp == '':
+            ec_temp = '-'
+        if ec_temp[0] == ',':
+            ec_temp = ec_temp[2:]
+        ecs.append(ec_temp)          
+
+        uniprot_temp = ', '.join(np.unique(uniprot_temp))
+        if uniprot_temp == '':
+            uniprot_temp = '-'
+        if uniprot_temp[0] == ',':
+            uniprot_temp = uniprot_temp[2:]
+        uniprots.append(uniprot_temp)
+
+    # Retrieving MEROPS IDs using UniProt REST API
+    url = 'https://www.uniprot.org/uploadlists/'
+    params = {'from': 'ACC+ID', 'to': 'MEROPS_ID', 'format': 'tab', 'query': ', '.join(uniprots)}
+    convertion = parse.urlencode(params)
+    convertion = convertion.encode('utf-8')
+    req = request.Request(url, convertion)
+    with request.urlopen(req) as f:
+       merops_temp = f.read()
+    merops_temp = merops_temp.decode('utf-8').split()[2:]
+
+    merops_part = [x for (i, x) in enumerate(merops_temp) if i in np.arange(1, len(merops_temp), 2)]
+    uniprot_part = [x for (i, x) in enumerate(merops_temp) if i in np.arange(0, len(merops_temp), 2)]
+    uniprot_merops_convertion = dict(zip(uniprot_part, merops_part))
+          
+    merops = []
+    for uniprot_temp in uniprots:
+        if uniprot_temp == '-':
+            merops.append('-')
+        else:
+            merops_temp = []
+            for uniprot_id in uniprot_temp.split(', '):
+                if uniprot_id in uniprot_part:
+                    merops_temp.append(uniprot_merops_convertion[uniprot_id])
+            merops_temp = ', '.join(merops_temp)
+            if merops_temp == '':
+                merops_temp = '-'
+            merops.append(merops_temp)
+
+    data['EC_number'] = ecs
+    data['UniProt_ID'] = uniprots
+    data['MEROPS_ID'] = merops
+    return(data)
+
+##################################################################################
+
+def find_catalytic_residues(data):
+    """
+    Function that finds catalytic residues
+    """
+    with open('homologues_residues.json', 'r') as f:
+        residues = f.readlines() 
+    residues = residues[0].split('}, {')
+    residues = [x.split('[{')[1] if len(x.split('[{')) > 1 else x for x in residues]
+
+    catalytic_residues = []
+    for (pdb, chains) in zip(data['PDB'].values, data['Protein_chains'].values):
+        cat_res_tmp = []
+        for res in residues:
+            if pdb in res:
+                res = [x.replace('"', '').replace(',', '') for (i, x) in enumerate(res.split()) if i in [1, 5, 9]]
+                if res[1] != 'null' and res[2] in chains:
+                     cat_res_tmp.append(res[2]+res[1]+res[0].upper())
+        cat_res_tmp = ', '.join(np.unique(cat_res_tmp))
+        if cat_res_tmp == '':
+            cat_res_tmp = '-'
+        catalytic_residues.append(cat_res_tmp)
+
+    data['Catalytic_residues'] = catalytic_residues
+    return(data)
+
+########################################################################################
+# Main
+########################################################################################
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--input', default = 'sample_data.csv', type = str, required = True,
+                    help = 'Dataset with protease-ligand pairs to annotate')
+parser.add_argument('--output', default = 'annotated_dataset.csv', type = str, required = False, 
+                    help='Path to save the annotated dataset')
+args = parser.parse_args()
+
+# Read the dataset
+data = pd.read_table(args.input, sep = ',', header = None, names = ['PDB', 'Ligand'])
+# Find ligand and protein chains
+data = find_chains(data)
+# Perform annotation of IDs
+data = find_ids(data)
+# Annotate catalytic residues
+data = find_catalytic_residues(data)
+# Save annotated dataset
+data.to_csv(args.output, sep = '\t', index = 0)
